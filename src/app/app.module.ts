09d12b1c832a723f695ea7020cc1c89f9f621921
vadiimXD <vadim123bg@gmail.com>
2024-02-22 01:15:58 +0200

    Test ngFor ?!?

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
 import { NavigationComponent } from './navigation/navigation.component';
+import { UserListComponent } from './user-list/user-list.component';
 
 
 @NgModule({
-  declarations: [AppComponent, NavComponent,NavigationComponent],
-  imports: [BrowserModule, AppRoutingModule],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [AppComponent, NavComponent,NavigationComponent, UserListComponent], //!Components
+  imports: [BrowserModule, AppRoutingModule], //!helpers  / utils
+  providers: [], //!Services
+  bootstrap: [AppComponent] //!App launcher
 })
 export class AppModule { }
---
+++ src/app/user-list/user-list.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+
+interface User {
+  name: string
+  age: number
+}
+
+@Component({
+  selector: 'app-user-list',
+  templateUrl: './user-list.component.html',
+  styleUrls: ['./user-list.component.css']
+})
+
+export class UserListComponent {
+  users: User[] = [
+    { name: "Pesho", age: 18 },
+    { name: "Misho", age: 19 },
+    { name: "Gosho", age: 20 },
+    { name: "Sasho", age: 21 }
+  ]
+}
